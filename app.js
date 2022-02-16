0c03a098601525c545040a13d68b3b7bcac184af
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-17 01:34:16 +0600

    new code added

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
 // this is for total expense
 function getTotalExpense() {
   const foodExpenseInput = document.getElementById("food-feild");
-  const foodExpense = foodExpenseInput.value;
+  const foodExpense = parseFloat(foodExpenseInput.value);
   const rentExpenseInput = document.getElementById("rent-feild");
-  const rentExpense = rentExpenseInput.value;
+  const rentExpense = parseFloat(rentExpenseInput.value);
   const clotheExpenseInput = document.getElementById("clothes-feild");
-  const clotheExpense = clotheExpenseInput.value;
-  const totalExpense =
-    parseFloat(foodExpense) +
-    parseFloat(rentExpense) +
-    parseFloat(clotheExpense);
-  return totalExpense;
+  const clotheExpense = parseFloat(clotheExpenseInput.value);
+
+  if (
+    // validation check
+    typeof foodExpense != "number" ||
+    typeof rentExpense != "number" ||
+    typeof clotheExpense != "number" ||
+    foodExpense < 0 ||
+    rentExpense < 0 ||
+    clotheExpense < 0
+  ) {
+    return "Something wrong in expense input";
+  } else {
+    const totalExpense = foodExpense + rentExpense + clotheExpense;
+    return totalExpense;
+  }
 }
 
 // this is for total balance
 function getBalance(totalExpense) {
   const incomeInput = document.getElementById("income-feild");
   const income = parseFloat(incomeInput.value);
-  const balance = income - totalExpense;
-
-  return balance;
+  if (typeof income != "number" || income < 0) {
+    //   validation
+    return "something wrong in income input";
+  } else {
+    if (income < totalExpense) {
+      //validation
+      return "not avaiable money";
+    } else {
+      const balance = income - totalExpense;
+      return balance;
+    }
+  }
 }
 
 // this is for calculate all value
 document.getElementById("cal-btn").addEventListener("click", function () {
   const totalExpense = getTotalExpense();
   const balance = getBalance(totalExpense);
+
   document.getElementById("total-expenses").innerText = totalExpense;
   document.getElementById("balance").innerText = balance;
 });
@@ -34,15 +54,27 @@ document.getElementById("cal-btn").addEventListener("click", function () {
 document.getElementById("save-btn").addEventListener("click", function () {
   //   this  save Persentage code
   const saveInput = document.getElementById("save-feild");
-  const savePersentage = saveInput.value;
+  const savePersentage = parseFloat(saveInput.value);
   const incomeInput = document.getElementById("income-feild");
   const income = parseFloat(incomeInput.value);
-  const saveValue = (income * parseFloat(savePersentage)) / 100;
-  document.getElementById("saving-amount").innerText = saveValue;
 
-  //this remaining balance code
-  const totalExpense = getTotalExpense();
-  const balance = getBalance(totalExpense);
-  const remainingBalance = balance - saveValue;
-  document.getElementById("remaining-balance").innerText = remainingBalance;
+  if (typeof savePersentage != "number" || savePersentage < 0) {
+    //   checking valid input
+    console.log("something wrong in save input");
+  } else {
+    const saveAmount = (income * savePersentage) / 100;
+    document.getElementById("saving-amount").innerText = saveAmount;
+
+    //this remaining balance code
+    const totalExpense = getTotalExpense();
+    const balance = getBalance(totalExpense);
+
+    if (saveAmount > balance) {
+      // validation
+      console.log("Not enough money to save");
+    } else {
+      const remainingBalance = balance - saveAmount;
+      document.getElementById("remaining-balance").innerText = remainingBalance;
+    }
+  }
 });
