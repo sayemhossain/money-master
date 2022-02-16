3faf799868475ceef2b2873523466c7acd662e97
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-17 00:54:42 +0600

    new code added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// this is for total expense
 function getTotalExpense() {
   const foodExpenseInput = document.getElementById("food-feild");
   const foodExpense = foodExpenseInput.value;
@@ -11,6 +12,8 @@ function getTotalExpense() {
     parseFloat(clotheExpense);
   return totalExpense;
 }
+
+// this is for total balance
 function getBalance(totalExpense) {
   const incomeInput = document.getElementById("income-feild");
   const income = parseFloat(incomeInput.value);
@@ -18,6 +21,8 @@ function getBalance(totalExpense) {
 
   return balance;
 }
+
+// this is for calculate all value
 document.getElementById("cal-btn").addEventListener("click", function () {
   const totalExpense = getTotalExpense();
   const balance = getBalance(totalExpense);
@@ -25,14 +30,17 @@ document.getElementById("cal-btn").addEventListener("click", function () {
   document.getElementById("balance").innerText = balance;
 });
 
+//adding save parsentages
 document.getElementById("save-btn").addEventListener("click", function () {
+  //   this  save Persentage code
   const saveInput = document.getElementById("save-feild");
   const savePersentage = saveInput.value;
-
   const incomeInput = document.getElementById("income-feild");
   const income = parseFloat(incomeInput.value);
   const saveValue = (income * parseFloat(savePersentage)) / 100;
   document.getElementById("saving-amount").innerText = saveValue;
+
+  //this remaining balance code
   const totalExpense = getTotalExpense();
   const balance = getBalance(totalExpense);
   const remainingBalance = balance - saveValue;
