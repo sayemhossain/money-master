1f2d4a4c46570836cf72d6b1ed8956b54257a911
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-16 23:52:45 +0600

    new code added

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("cal-btn").addEventListener("click", function () {
+  const incomeInput = document.getElementById("income-feild");
+  const income = incomeInput.value;
+  const foodExpenseInput = document.getElementById("food-feild");
+  const foodExpense = foodExpenseInput.value;
+  const rentExpenseInput = document.getElementById("income-feild");
+  const rentExpense = rentExpenseInput.value;
+  const clotheExpenseInput = document.getElementById("income-feild");
+  const clotheExpense = clotheExpenseInput.value;
+});
