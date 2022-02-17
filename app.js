6d90ce69c8f0fb3397dd8e5aeb8df28071b0b98f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-17 06:05:25 +0600

    Alhamdulillah it's done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// this is for total expense
+// this is the function for total expenses
 function getTotalExpense() {
   const foodExpenseInput = document.getElementById("food-feild");
   const foodExpense = foodExpenseInput.value;
@@ -8,24 +8,58 @@ function getTotalExpense() {
   const clotheExpense = clotheExpenseInput.value;
 
   if (foodExpense < 0 || rentExpense < 0 || clotheExpense < 0) {
+    // checking validation for negative value
     return 1;
   } else if (isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clotheExpense)) {
+    // checking validation for number input
     return 2;
   } else {
+    // here I return total expenses
     const totalExpense =
       parseFloat(foodExpense) +
       parseFloat(rentExpense) +
       parseFloat(clotheExpense);
-    console.log(totalExpense);
     return totalExpense;
   }
 }
 
-// this is for total balance
+// this is for calculate income and expenses value
+document.getElementById("cal-btn").addEventListener("click", function () {
+  // this is for total expense
+  const totalExpense = getTotalExpense();
+  if (totalExpense == 1) {
+    document.getElementById("error").innerText =
+      "Please enter positive number...!";
+    document.getElementById("total-expenses").innerText = 0;
+  } else if (totalExpense == 2) {
+    document.getElementById("error").innerText = "Please enter only number...!";
+    document.getElementById("total-expenses").innerText = 0;
+  } else {
+    document.getElementById("total-expenses").innerText = totalExpense;
+    document.getElementById("error").innerText = "";
+  }
+  // this is for balance
+  const balance = getBalance(totalExpense);
+  if (balance == -1) {
+    document.getElementById("error").innerText =
+      "Please enter positive number...!";
+    document.getElementById("balance").innerText = 0;
+  } else if (balance == -2) {
+    document.getElementById("error").innerText = "Please enter only number...!";
+    document.getElementById("balance").innerText = 0;
+  } else if (balance == -3) {
+    document.getElementById("error").innerText =
+      "You don't have enough money...!";
+    document.getElementById("balance").innerText = 0;
+  } else {
+    document.getElementById("balance").innerText = balance;
+  }
+});
+
+// this is the function for total balance
 function getBalance(totalExpense) {
   const incomeInput = document.getElementById("income-feild");
   const income = incomeInput.value;
-  console.log(income);
   if (income < 0) {
     return -1;
   } else if (isNaN(income)) {
@@ -38,65 +72,32 @@ function getBalance(totalExpense) {
   }
 }
 
-// this is for calculate all value
-document.getElementById("cal-btn").addEventListener("click", function () {
-  // this is for total expense
-  const totalExpense = getTotalExpense();
-  if (totalExpense == 1) {
-    document.getElementById("error").innerText =
-      "Please enter positive number!";
-    document.getElementById("total-expenses").innerText = 0;
-  } else if (totalExpense == 2) {
-    document.getElementById("error").innerText = "Please enter only number!";
-    document.getElementById("total-expenses").innerText = 0;
-  } else {
-    document.getElementById("total-expenses").innerText = totalExpense;
-    document.getElementById("error").innerText = "";
-  }
-  //   this is for balance
-  const balance = getBalance(totalExpense);
-  if (balance == -1) {
-    document.getElementById("error").innerText =
-      "Please enter valid number! Negative value isn't allowed.";
-    document.getElementById("balance").innerText = "";
-  } else if (balance == -2) {
-    document.getElementById("error").innerText = "Please enter only number!";
-    document.getElementById("balance").innerText = "";
-  } else if (balance == -3) {
-    document.getElementById("error").innerText = "You don't have enough money!";
-    document.getElementById("balance").innerText = "";
-  } else {
-    document.getElementById("balance").innerText = balance;
-    document.getElementById("error").innerText = "";
-  }
-});
-
-//adding save parsentages
+// adding saveing parsentages
 document.getElementById("save-btn").addEventListener("click", function () {
-  //   this  save Persentage code
+  //  this is for save Persentage
   const saveInput = document.getElementById("save-feild");
-  const savePersentage = parseFloat(saveInput.value);
+  const savePersentage = saveInput.value;
   const incomeInput = document.getElementById("income-feild");
   const income = parseFloat(incomeInput.value);
 
   if (isNaN(savePersentage)) {
-    //   checking valid input
+    // checking valid input
     document.getElementById("saveing-error").innerText =
-      "Please enter only number!";
+      "Please enter only number...!";
   } else if (savePersentage < 0) {
     document.getElementById("saveing-error").innerText =
-      "Please enter positive number!";
+      "Please enter positive number...!";
   } else {
-    const saveAmount = (income * savePersentage) / 100;
+    const saveAmount = (income * parseFloat(savePersentage)) / 100;
     document.getElementById("saving-amount").innerText = saveAmount;
 
-    //this remaining balance code
+    // this remaining balance code
     const totalExpense = getTotalExpense();
     const balance = getBalance(totalExpense);
 
     if (saveAmount > balance) {
       document.getElementById("saveing-error").innerText =
-        "Not enough money for saveing!";
+        "Not enough money for saveing...!";
     } else {
       const remainingBalance = balance - saveAmount;
       document.getElementById("remaining-balance").innerText = remainingBalance;
